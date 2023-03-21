1f432b69b7735c8d3b16eed40b2b957ba04027ee
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-03-21 20:00:51 +0530

    add relationship from blogs to user and vice-versa

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
 
 app = FastAPI()
 
+# routes
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Sarthak'}}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog')  # decorator
+# path operation function
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # return published
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def drafts():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def about(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of note with id
+    return limit
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'Blog is created with {request.title}'}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
