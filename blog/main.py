6031217312e994b17c6322525a9469b7b9b32365
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-03-22 15:19:40 +0530

    create login router

--- blog/main.py
+++ blog/main.py
@@ -2,29 +2,17 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 import uvicorn
-from .routers import blog, user
+from .routers import blog, user, authentication
 
 app = FastAPI()
 
 # create all the models to the database
 models.Base.metadata.create_all(engine)
 
+app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
-# @app.get('/blog', response_model=List[schemas.ShowBlog, ], tags=['blogs'])
-# def all(db: Session = Depends(get_db)):
-#     blogs = db.query(models.Blog).all()
-#     return blogs
 
 
 if __name__ == '__main__':
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 from typing import List
 
+class Login(BaseModel):
+    username: str
+    password: str
 
 class BlogBase(BaseModel):
     title: str
